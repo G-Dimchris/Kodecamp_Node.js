4eeaf6f4e8e46ecabf3f0259f8a5ead41e36e0d3
dimchris <adimchichristopher@gmail.com>
2026-05-23 22:47:06 +0100

    adding more features

---
+++ task 2/app.js
@@ -0,0 +1,32 @@
+//const evenNumbers = [];
+
+//do {
+//    const num = Math.Floor(Math.random() * 10);
+//    if (num % 2 == 0) {
+//        evenNumbers.push(num);
+//    }
+//} while(evenNumbers.length == 0);
+
+//console.log(evenNumbers);
+
+let dayOfTheWeek = "Monday";
+
+switch (dayOfTheWeek) {
+    case "Monday":
+        console.log("Today is Monday");
+        break;
+    case "Tuesday":
+        console.log("Today is Tuesday");
+        break;
+    case "Wednesday":
+        console.log("Today is Wednesday");
+        break;
+    case "Thursday":
+        console.log("Today is Thursday");
+        break;
+    case "Friday":
+        console.log("Today is Friday");
+        break;
+    default:
+        console.log("Invalid day of the week");
+}
