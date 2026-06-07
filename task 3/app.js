1140e7562a4e329229cea658f13dff92eadaec6d
dimchris <adimchichristopher@gmail.com>
2026-06-07 20:30:27 +0100

    update task 3

--- task 3/app.js
+++ task 3/app.js
@@ -1,16 +1,13 @@
 // QUESTION 1
 // Function to get GitHub user details
 
-async function getGitHubUser(GDimchris) {
+async function getGitHubUser() {
   try {
-    const response = await fetch("https://api.github.com/users/GDimchris");
-
-    // Check if user exists
+    const response = await fetch("https://api.github.com/users/G-Dimchris");
     if (!response.ok) {
-      throw new Error("GitHub user not found");
+      throw new Error("Username not found");
     }
 
-    // Convert response to JSON
     const user = await response.json();
 
     return user;
@@ -19,104 +16,5 @@ async function getGitHubUser(GDimchris) {
   }
 }
 
-/* // Example usage
-getGitHubUser("octocat")
-  .then(user => console.log(user)); */
-
-
-
-
-async function getGitHubUser(username) {
-  try {
-    const response = await fetch(
-      `https://api.github.com/users/${username}`
-    );
-
-    if (!response.ok) {
-      throw new Error("User not found");
-    }
-
-    const user = await response.json();
-
-    console.log("User Details:");
-    console.log("Name:", user.name);
-    console.log("Username:", user.login);
-    console.log("Followers:", user.followers);
-    console.log("Following:", user.following);
-    console.log("Public Repos:", user.public_repos);
-    console.log("Profile URL:", user.html_url);
-
-    return user;
-  } catch (error) {
-    console.error(error.message);
-  }
-}
-
-getGitHubUser("octocat");
-
-
-// QUESTION 2
-
-class KCStore {
-  constructor() {
-    this.products = [];
-  }
-
-  addProduct(product) {
-    this.products.push(product);
-    console.log(`${product.name} added successfully`);
-  }
-
-  viewProducts() {
-    console.log(this.products);
-  }
-
-  updateProduct(id, updatedData) {
-    const product = this.products.find(
-      (item) => item.id === id
-    );
-
-    if (!product) {
-      console.log("Product not found");
-      return;
-    }
-
-    Object.assign(product, updatedData);
-
-    console.log("Product updated successfully");
-  }
-
-  deleteProduct(id) {
-    this.products = this.products.filter(
-      (item) => item.id !== id
-    );
-
-    console.log("Product deleted successfully");
-  }
-}
-
-// Testing
-
-const store = new KCStore();
-
-store.addProduct({
-  id: 1,
-  name: "Laptop",
-  price: 500000
-});
-
-store.addProduct({
-  id: 2,
-  name: "Phone",
-  price: 200000
-});
-
-store.viewProducts();
-
-store.updateProduct(1, {
-  price: 550000
-});
-
-store.deleteProduct(2);
-
-store.viewProducts();
+getGitHubUser("G-Dimchris")
+  .then(user => console.log(user));
